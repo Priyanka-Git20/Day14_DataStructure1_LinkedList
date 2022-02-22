8eddc9605a084a8d195864e6decb587a9dba171d
priyanka <priyanka.chavan1408@gmail.com>
2022-02-22 14:27:23 +0530

    ADDED:Create a simple linked list.

---
+++ MyNode.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.datastructure;
+
+public class MyNode<K> {
+
+    private K key;
+    private MyNode next;
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+}
+
+
+
