dfcc38a24263aeea89296790a4f2597a0188993b
priyanka <priyanka.chavan1408@gmail.com>
2022-02-22 15:22:57 +0530

    ADDED:Create a simple linked list and add 56>30>70.

--- MyNode.java
+++ MyNode.java
@@ -1,23 +1,32 @@
 package com.bridgelabz.datastructure;
 
-public class MyNode<K> {
+public class MyNode<K> implements INode<K> {
 
     private K key;
-    private MyNode next;
-
-    public MyNode getNext() {
-        return next;
-    }
-
-    public void setNext(MyNode next) {
-        this.next = next;
-    }
+    private INode next;
 
     public MyNode(K key) {
         this.key = key;
         this.next = null;
     }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public INode<K> getNext() {
+        return next;
+    }
+
+    @Override
+    public void setNext(INode next) {
+        this.next = (MyNode<K>) next;
+    }
 }
-
-
 
